b052c489f8830cc0af382d820fc7d15b0308b8c8
SeongMin-K <eo1tks3@naver.com>
2022-01-19 15:56:36 +0900

    Apply MVC Pattern

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,16 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        storyLabel.text = storyBrain.stories[0].title
+        choice1Button.setTitle(storyBrain.stories[0].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[0].choice2, for: .normal)
     }
-
-
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
+    }
 }
-
